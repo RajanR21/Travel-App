4dad9146d7c2d8cb846fc6946b2407f7bb86df7b
Rajan Ravisaheb <rajanroy220@gmail.com>
2025-04-07 19:05:38 +0200

    route protection added

--- client/src/App.js
+++ client/src/App.js
@@ -10,22 +10,72 @@ import AllPackages from "./pages/package/AllPackages";
 import Package from "./pages/package/Package";
 import AllFlights from "./pages/flights/AllFlights";
 import Flight from "./pages/flights/Flight";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Toaster />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hotels" element={<List />} />
-        <Route path="/hotels/:id" element={<Hotel />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hotels"
+          element={
+            <ProtectedRoute>
+              <List />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/hotels/:id"
+          element={
+            <ProtectedRoute>
+              <Hotel />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<Signin />} />
         <Route path="/register" element={<Signup />} />
-        <Route path="/verify-otp" element={<VerifyOtp />} />
-        <Route path="/packages" element={<AllPackages />} />
-        <Route path="/packages/:id" element={<Package />} />
-        <Route path="/flights" element={<AllFlights />} />
-        <Route path="/flights/:id" element={<Flight />} />
+        {/* <Route path="/verify-otp" element={<VerifyOtp />} /> */}
+        <Route
+          path="/packages"
+          element={
+            <ProtectedRoute>
+              <AllPackages />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/packages/:id"
+          element={
+            <ProtectedRoute>
+              <Package />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/flights"
+          element={
+            <ProtectedRoute>
+              <AllFlights />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/flights/:id"
+          element={
+            <ProtectedRoute>
+              <Flight />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
